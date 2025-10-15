8abe810e64b197f245d684fccfdcb91290fa8c09
Jadiael <Jadiael@hotmail.com.br>
2025-10-15 12:42:16 -0300

    feat: add tile action to warp guests into personal woods

---
+++ PerFarmhandWoods/TileActions/EnterWoodsAction.cs
@@ -0,0 +1,30 @@
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace PerFarmhandWoods.TileActions
+{
+    public sealed class EnterWoodsAction
+    {
+        private readonly IMonitor _monitor;
+        private readonly string _baseWoodsName = Helpers.Constants.BaseWoodsName;
+
+        private readonly string _enterWoodsActionKey = Helpers.Constants.EnterWoodsActionKey;
+
+        public EnterWoodsAction(IMonitor monitor)
+        {
+            _monitor = monitor;
+            GameLocation.RegisterTouchAction(_enterWoodsActionKey, this.OnEnterWoods);
+        }
+
+        private void OnEnterWoods(GameLocation location, string[] args, Farmer player, Vector2 tile)
+        {
+            if (args.Length != 3 || args[0] != _enterWoodsActionKey) return;
+            if (!int.TryParse(args[1], out int x) || !int.TryParse(args[2], out int y)) return;
+            if (Context.IsMainPlayer) return;
+
+            string target = $"{_baseWoodsName}{player.UniqueMultiplayerID}";
+            Game1.warpFarmer(target, x, y, player.FacingDirection);
+        }
+    }
+}
